3e86ac9eb58058533d84d25fe709f0ec503b22d4
irwingd <irwing_d@yahoo.co.in>
2025-08-24 22:35:43 +0530

    removed dotenv

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+﻿from fastapi import FastAPI
 from pydantic import BaseModel
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph import StateGraph, END
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', temperature=0)
 
